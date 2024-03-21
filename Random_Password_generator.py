3c417778d5884c0367801bf0383b535adcdaab89
AYUSHDEVANI <ayushdevani0018@gmail.com>
2024-03-21 23:07:58 +0530

    Make a function for password generate according to length and other arguments.

--- Random_Password_generator.py
+++ Random_Password_generator.py
@@ -1,2 +1,20 @@
 import random
 import string
+
+def generate_password(length, use_letters=True, use_numbers=True, use_symbols=True):
+    characters = ""
+    if use_letters:
+        characters += string.ascii_letters
+
+    if use_numbers:
+        characters += string.digits
+
+    if use_symbols:
+        characters += string.punctuation
+
+    password = "".join(random.choice(characters) for _ in range(length))
+
+    return password
+
+    
+
